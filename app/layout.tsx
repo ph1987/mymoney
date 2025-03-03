1a86a1194ad5db6ec5279f24bf319648b0222175
phil <ph1987@gmail.com>
2025-03-03 14:32:08 -0300

    google site verifiation

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+			<head>
+				<meta name="google-site-verification" content="foqe4HjA6gAimCND9PzjyCa60WDcqRUn6uTEWEjdzQE" />
+			</head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
